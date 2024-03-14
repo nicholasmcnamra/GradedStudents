e72126d321855e272a0d8560574d51170809751a
nicholasmcnamra <nikomcnamara@gmail.com>
2024-03-14 15:04:32 -0400

    built out student methods up to toString() override and three tests

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,66 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+
+    private String firstName;
+
+    private String lastName;
+
+    private ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, ArrayList<Double> examScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = examScores;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getExamScores(ArrayList examScores) {
+        String examScoreList = "Exam Scores: ";
+        for (int i = 0; i < examScores.size(); i++) {
+            examScoreList += ("\n" + examScores.get(i));
+        }
+
+        return examScoreList;
+    }
+
+    public void addExamScore(double examScore) {
+        examScores.add(examScore);
+    }
+
+    //There needs to be a better way to write this one. I know it's wrong..
+    public void setExamScore(double examIndex, double newScore) {
+                examScores.set(0, newScore);
+            }
+
+    public double getAverageExamScore(ArrayList examScores) {
+        double averageExamScore = 0;
+        for (int i = 0; i < examScores.size(); i++) {
+            averageExamScore += ((Double) examScores.get(i));
+        }
+        averageExamScore = averageExamScore / examScores.size();
+        return averageExamScore;
+    }
+
+    public int getNumberOfExamsTaken(ArrayList examScores) {
+        return examScores.size();
+    }
+
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,62 @@
 package io.zipcoder;
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.util.ArrayList;
 
 public class StudentTest {
 
-}
+    @Test
+    public void getExamScoresTest() {
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        ArrayList<Double> examScores = new ArrayList<>();
+        examScores.add(100.0);
+        examScores.add(95.0);
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        String expectedExamScores = "Exam Scores: \n" + 100.0 + "\n" + 95.0;
+        String actualExamScores = student.getExamScores(examScores);
+
+        // Then
+        Assert.assertEquals(expectedExamScores, actualExamScores);
+        }
+
+        @Test
+        public void addExamScoreTest() {
+            // : Given
+            String firstName = "Leon";
+            String lastName = "Hunter";
+            ArrayList<Double> examScores = new ArrayList<>();
+            examScores.add(100.0);
+            Student student = new Student(firstName, lastName, examScores);
+
+            // When
+            String expectedExamScore = "Exam Scores: \n" + 100.0;
+            String actualExamScore = student.getExamScores(examScores);
+
+            // Then
+            Assert.assertEquals(expectedExamScore, actualExamScore);
+        }
+
+        @Test
+        public void setExamScoreTest() {
+            // : Given
+            String firstName = "Leon";
+            String lastName = "Hunter";
+            ArrayList<Double> examScores = new ArrayList<>();
+            examScores.add(100.0);
+            Student student = new Student(firstName, lastName, examScores);
+            student.setExamScore(0, 150.0);
+            // When
+            String expectedExamScore = "Exam Scores: \n" + 150.0;
+            String actualExamScore = student.getExamScores(examScores);
+
+            // Then
+            Assert.assertEquals(expectedExamScore, actualExamScore);
+        }
+
+        
+    }
